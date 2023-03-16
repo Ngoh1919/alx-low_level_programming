377041d52f53ed9c88cc328be27f7dbecfb22d35
myname <myname>
2023-03-16 13:11:20 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * This is a code that prints Programming is like building a multilingual puzzle
+ * and after it will return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * this is a progra to print with proper grammar, but the outcome is a piece of art, using printf 
+ * return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
